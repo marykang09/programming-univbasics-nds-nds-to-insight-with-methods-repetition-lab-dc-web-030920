8525674283a1336153eb24786d5b66ac2f385bb7
Mary Kang <mary.kang09@gmail.com>
2020-03-07 18:40:51 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -54,7 +54,7 @@ def total_gross(source)
   
   while index < director_names.length do
     director_names = director_names[index]
-    grand_total += directors_grand_total[director_names]
+    grand_total += directors_grand_total[list_of_directors(source)]
     index += 1 
 end
 grand_total
