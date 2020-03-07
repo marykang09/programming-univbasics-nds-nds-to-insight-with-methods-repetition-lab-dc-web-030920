ba05b1d536f332d11092e53b6f14f706b5200f33
Mary Kang <mary.kang09@gmail.com>
2020-03-07 18:44:31 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -46,7 +46,7 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
   
-  director = directors_totals(source)
+  dir_to_earnings_hash = directors_totals(source)
   dir_names = list_of_directors(source)
   i = 0
   total = 0
